83c4cc799bd40e4ab07ea3858a5a19d759460613
Daniel <ddelacruzz@gmail.com>
2022-07-23 00:33:50 -0500

    Added responsive sidebard

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
-import { AppThemeProvider } from './shared/contexts';
+import { Sidebard } from './shared/components';
+import { AppThemeProvider, DrawerProvider } from './shared/contexts';
 
 
 export const App = () => {
   return (
     <AppThemeProvider>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
+      <DrawerProvider>
+        <BrowserRouter>
+          <Sidebard>
+            <AppRoutes />
+          </Sidebard>
+        </BrowserRouter>
+      </DrawerProvider>
     </AppThemeProvider>
   );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,16 @@
 import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { useAppThemeContext } from '../shared/contexts';
+import { useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
-  const { toggleTheme } = useAppThemeContext();
+  const { toggleDrawerOpen } = useDrawerContext();
   return (
     <Routes>
       <Route
         path="/login"
         element={
-          <Button variant="contained" color="primary" onClick={toggleTheme}>
-            change Theme
+          <Button variant="contained" color="primary" onClick={toggleDrawerOpen}>
+            change Drawer
           </Button>
         }
       ></Route>
